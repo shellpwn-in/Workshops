21c3e997065b610a3812bff64ea51e49f43b0ee3
Sahil Dharme <86155751+ash-S26@users.noreply.github.com>
2022-02-20 10:25:10 +0530

    Add files via upload

---
+++ python/library.py
@@ -0,0 +1,12 @@
+"""
+#Normal way
+pi = 3.14      # need to remember 
+r = 2
+print("Area is ",pi*(r**2))
+"""
+import math
+pi = math.pi
+r = 2
+print("pi value is",pi)
+print("Area is ",pi*(r**2))
+
