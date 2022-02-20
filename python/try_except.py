551ad104cd4d7355c43129a7553b3cd01bed7178
Sahil Dharme <86155751+ash-S26@users.noreply.github.com>
2022-02-20 10:17:39 +0530

    Add files via upload

---
+++ python/try_except.py
@@ -0,0 +1,9 @@
+name = input("Enter your name : ")
+#age = input("Enter your age : ")
+age = int(input("Enter your age : "))
+
+try:
+	print("Your name is " + name + " and age is " + age)
+	
+except :
+	print("An error has occoured")
